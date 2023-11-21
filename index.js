7f4cb98c23f546f33ab59f28dd0bca7925b97985
EvgenShaman <budil.evgen@gmail.com>
2023-11-21 12:42:00 +0300

    commit 2
    dfawrgaethaethjnr

--- index.js
+++ index.js
@@ -1,14 +1,41 @@
 const express = require('express');
-const {graphqlHTTP} = require("express-graphql")
-const port = 1235;
-const schema = require('C:\\Users\\evgen\\Downloads\\Telegram Desktop\\A6\\A6\\schema\\schema.js');
-const app = express()
+const axios = require('axios');
+const app = express();
+const port = 3000;
 
-app.use(
-    "/graphql",
-    graphqlHTTP({
-        schema: schema,
-        graphiql: true,
-    })
-)
-app.listen(port);
+
+async function getApiResponse(url) {
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+
+let episodes = [];
+
+app.get('/', (req, res) => {
+    let episodeName = `My Little Pony: Episode ${Math.floor(Math.random() * 100) + 1}`;
+    episodes.push(episodeName);
+
+    res.json({ episodeName: episodeName });
+});
+
+app.get('/all', (req, res) => {
+  res.json(episodes);
+});
+app.get('/get', async (req, res) => {
+    try {
+        const response = await getApiResponse("https://ponyapi.net/v1/character/1");
+        res.json(response);
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('An error occurred while fetching data from the API');
+    }
+});
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}/`);
+});
